02cb9a23f98764118e79ed531a283abd93a302cd
pestich <pestich@mail.ru>
2023-01-03 14:26:21 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+from views import main_bp
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+
+    app.register_blueprint(main_bp)
+
+    return app
+
+# app = Flask(__name__)
+# app.config['JSON_AS_ASCII'] = False
+# app.register_blueprint(main_bp)
+
+
